83b53ee8ae68a2a4de4c7216aae7770a0774fa7c
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-10-10 15:29:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+import { Activity } from 'react-bootstrap-icons';
+import { UniversalAccess } from 'react-bootstrap-icons';
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState(0)
+  const [lower, setlower] = useState(0)
+  const [upper, setUpper] = useState(0)
+
+  const calcLimits = () => {
+    if (age <= 0 || isNaN(age)) {
+      setlower(0)
+      setUpper(0)
+    } else {
+      const y = (220 - age) * 0.85
+      const x = (220 - age) * 0.65
+      setlower(x.toFixed(0))
+      setUpper(y.toFixed(0))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h2>Heart rate limits calculator</h2>
+      <form>
+        <div>
+          <label>Age <UniversalAccess /></label>
+          <input type="text" placeholder="40" value={age} onChange={e => setAge(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Heart rate limits <Activity /></label>
+          <output>{lower} - {upper}</output>
+        </div>
+        <Button variant="outline-primary" onClick={calcLimits}>Calculate</Button>{' '}
+      </form>
     </div>
   );
 }
